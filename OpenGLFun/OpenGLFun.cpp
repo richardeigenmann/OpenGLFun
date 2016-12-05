31094b86d1e07c987e4993ff478d2f74e580a93c
richardeigenmann <richard.eigenmann@gmail.com>
2016-12-05 20:41:45 +0100

    Working code!

--- OpenGLFun/OpenGLFun.cpp
+++ OpenGLFun/OpenGLFun.cpp
@@ -2,10 +2,41 @@
 //
 
 #include "stdafx.h"
+#include <windows.h>
+#include <GL/glut.h>
+#include <iostream>
 
+using std::cout;
 
-int main()
+auto initGl() -> void {
+	cout << "initGl\n";
+	glClearColor(0.3f, 1.0f, 0.0f, 1.0f);
+}
+
+auto drawPoints() -> void {
+	glBegin(GL_POINTS);
+	glColor3f(0.0f, 1.0f, 0.1f);
+	glVertex2f(0.1f, -0.6f);
+	glVertex2f(0.7f, -0.6f);
+	glVertex2f(0.4f, -0.1f);
+	glEnd();
+}
+
+auto update() -> void {
+	glClear(GL_COLOR_BUFFER_BIT);
+	drawPoints();
+	glFlush();
+}
+
+int main(int argc, char **argv)
 {
+	glutInit(&argc, argv);
+	glutCreateWindow("Richi GL Fun 2");
+	glutInitWindowSize(220, 220);
+	glutInitWindowPosition(20, 20);
+	glutDisplayFunc(update);
+	//initGl();
+	glutMainLoop();
     return 0;
 }
 
